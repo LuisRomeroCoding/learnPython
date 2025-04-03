08b00b4310137b1ae409b5fddd953e18a9dc7092
LuisRomeroCoding <luisromerocoding@gmail.com>
2025-04-02 18:30:14 -0600

    Mi primer hola mundo

---
+++ holaMundo.py
@@ -0,0 +1,11 @@
+# 1. Crea una nueva carpeta para tu proyecto.
+# 2. Dento, crea un archivo de nombre holaMundo.py, recuerda respetar la extension (.py) para que
+#    la computadora sepa que vas a escribir codigo de python dentro de este.
+# 3. Dentro de este nuevo archivo escribiras print("Hola Mundo")
+# 4. Da click en terminal y new terminal
+# 5. La terminal se abrira y en ella escribiras el siguiente comando python holaMundo.py
+# 6. Cada que necesites ejecutar tu codigo y ver su resultado tendras que correr el
+#    comando anterior (python nombreDeMiArchivo.py)
+# 7. Felicidades!!! Has escrito tu primer programa en python 
+
+print("Hola Mundo soy Beba")
